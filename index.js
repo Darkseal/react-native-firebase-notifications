7e2fe06c99c4aa0837512f6db8ff1f6636b7969d
Darkseal <games@myst.it>
2019-12-21 02:56:55 +0100

    Udpated to Tutorial Part #4.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './App_FireBase';
+// import App from './App_WebServer';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
